e68a687525b6e5e81b291ed9febabc8d0a00d43a
Ankoor <ankoorb@users.noreply.github.com>
2021-06-28 14:53:46 -0700

    Add files via upload

---
+++ Udacity-CPP-Nanodegree/2.6.ParseLines/cmake-build-debug/CMakeFiles/2_6_ParseLines.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2_6_ParseLines.dir/main.cpp.o"
+  "2_6_ParseLines.pdb"
+  "2_6_ParseLines"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_6_ParseLines.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Udacity-CPP-Nanodegree/2.7.UsingParseLine/cmake-build-debug/CMakeFiles/2_7_UsingParseLine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2_7_UsingParseLine.dir/main.cpp.o"
+  "2_7_UsingParseLine.pdb"
+  "2_7_UsingParseLine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_7_UsingParseLine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
