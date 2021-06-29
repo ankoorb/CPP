174ee0a51a4736227b036e85cd834deaa72d9b3a
Ankoor <ankoorb@users.noreply.github.com>
2021-06-29 14:56:59 -0700

    Add files via upload

---
+++ Udacity-CPP-Nanodegree/2.12.A-StarAddToOpen/main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+enum class State {
+    kEmpty,
+    kObstacle,
+    kClosed
+};
+
+string cellString(State cell);
+vector<State> parseLine(string line);
+vector<vector<State>> readBoardFile(string path);
+void printBoard(const vector<vector<State>> board);
+vector<vector<State>> Search(vector<vector<State>> board, int init[2], int goal[2]);
+int heuristic(int x1, int y1, int x2, int y2);
+void addToOpen(int x, int y, int g, int h, vector<vector<int>>& open_node, vector<vector<State>>& grid);
+
+
+int main() {
+
+    string path = "../1.board";
+    vector<vector<State>> board = readBoardFile(path);
+
+    int init[2] = {0, 0};
+    int goal[2] = {4, 5};
+
+    vector<vector<State>> solution = Search(board, init, goal);
+
+    printBoard(solution);
+
+
+    return 0;
+}
+
+vector<State> parseLine(string line){
+    int n;
+    char c;
+    istringstream l_stream(line);
+    vector<State> output;
+
+    while (l_stream >> n >> c && c == ','){
+        output.emplace_back(n == 0 ? State::kEmpty : State::kObstacle);
+    }
+
+    return output;
+}
+
+vector<vector<State>> readBoardFile(string path){
+
+    ifstream in_file(path);
+    vector<vector<State>> board;
+
+    if (in_file){
+        string line;
+        while (getline(in_file, line)){
+            board.emplace_back(parseLine(line));
+        }
+    }
+
+    return board;
+}
+
+void printBoard(const vector<vector<State>> board){
+    for (auto row: board){
+        for (auto v: row){
+            cout << cellString(v) << flush;
+        }
+        cout <<  endl;
+    }
+}
+
+string cellString(State cell){
+    switch (cell){
+        case State::kObstacle:
+            return "⛰️  ";
+        default:
+            return "0   ";
+    }
+}
+
+vector<vector<State>> Search(vector<vector<State>> board, int init[2], int goal[2]){
+    return board;
+}
+
+int heuristic(int x1, int y1, int x2, int y2){
+    return abs(x1 - x2) + abs(y1 - y2);
+}
+
+void addToOpen(int x, int y, int g, int h, vector<vector<int>>& open_node, vector<vector<State>>& grid){
+    vector<int> node = {x, y, g, h};
+    open_node.emplace_back(node);
+    grid[x][y] = State::kClosed;
+}
+
---
+++ Udacity-CPP-Nanodegree/2.13.A-StarSearch/cmake-build-debug/CMakeFiles/2_13_A_StarSearch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2_13_A_StarSearch.dir/main.cpp.o"
+  "2_13_A_StarSearch.pdb"
+  "2_13_A_StarSearch"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_13_A_StarSearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
