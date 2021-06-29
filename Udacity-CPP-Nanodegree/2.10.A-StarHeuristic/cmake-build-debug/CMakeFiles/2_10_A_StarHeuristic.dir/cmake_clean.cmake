d8fa6231875ed2044b83890ea11b2de12d75e844
Ankoor <ankoorb@users.noreply.github.com>
2021-06-29 14:55:35 -0700

    Add files via upload

---
+++ Udacity-CPP-Nanodegree/2.10.A-StarHeuristic/cmake-build-debug/CMakeFiles/2_10_A_StarHeuristic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2_10_A_StarHeuristic.dir/main.cpp.o"
+  "2_10_A_StarHeuristic.pdb"
+  "2_10_A_StarHeuristic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_10_A_StarHeuristic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
