aa7db9e8634a1cef5a19e9647fb9cdb18de81daa
Ankoor <ankoorb@users.noreply.github.com>
2021-06-29 14:54:10 -0700

    Add files via upload

---
+++ Udacity-CPP-Nanodegree/2.8.FormatPrintedBoard/cmake-build-debug/CMakeFiles/2_8_FormatPrintedBoard.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2_8_FormatPrintedBoard.dir/main.cpp.o"
+  "2_8_FormatPrintedBoard.pdb"
+  "2_8_FormatPrintedBoard"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_8_FormatPrintedBoard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
